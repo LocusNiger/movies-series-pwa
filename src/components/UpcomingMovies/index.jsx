4f71afef48b6633dfbbd49e1ea4649b14e7faf97
Iván Sánchez <sanchezivan.798@gmail.com>
2022-08-25 17:35:52 -0300

    Added: Upcoming movies in TrendingMovies section

---
+++ src/components/UpcomingMovies/index.jsx
@@ -0,0 +1,47 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const UpcomingMovies = () => {
+  const [upcoming, setUpcoming] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://api.themoviedb.org/3/movie/upcoming?api_key=9cc2ccd6d9648c49e03bee3c9b88a569&language=en-US&page=1`)
+      .then((res) => {
+        setUpcoming(res.data.results);
+        console.log(res.data.results);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  upcoming.splice(5, 19);
+  return (
+    <>
+      <section className="mx-auto my-10 max-w-7xl">
+        <div className="max-w-3xl mx-auto text-center">
+          <h1 className="text-3xl font-extrabold text-transparent sm:text-5xl bg-clip-text bg-gradient-to-r from-gray-300 via-fuchsia-600 to-orange-600 h-20">
+            Upcoming movies
+          </h1>
+        </div>
+        <ul className="grid grid-cols-1 gap-10 box-border justify-items-center sm:grid-cols-2 sm:gap-x-0.5 sm:gap-y-8 lg:grid-cols-5">
+          {upcoming.map((movie) => (
+            <li key={movie.id} className="shrink-0 snap-center">
+              <Link to={`/movie-detail?movieId=${movie.id}`}>
+                <img
+                  className="w-72 lg:w-52 rounded-xl"
+                  src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                  alt={`${movie.title} poster`}
+                />
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </>
+  );
+};
+
+export default UpcomingMovies;
--- src/pages/TrendingMovies/index.jsx
+++ src/pages/TrendingMovies/index.jsx
@@ -1,14 +1,11 @@
-import { Link, Navigate, useNavigate } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
+import UpcomingMovies from "../../components/UpcomingMovies";
 import { useMovies } from "../../hooks/useMovies";
-import { usePopularMovies } from "../../hooks/usePopularMovies";
 
 const TrendingMovies = (props) => {
   const { moviesTrending } = useMovies();
-  const { popularMovies } = usePopularMovies();
-  const navigate = useNavigate();
 
   moviesTrending.splice(10, 19);
-  popularMovies.splice(10, 19);
   return (
     <>
       {props.isLogged == false && <Navigate to="/" />}
@@ -33,27 +30,8 @@ const TrendingMovies = (props) => {
           ))}
         </ul>
       </section>
-      {/* Popular Movies */}
-      <section className="mx-auto my-4 max-w-7xl">
-        <div className="max-w-3xl mx-auto text-center">
-          <h2 className="text-3xl font-extrabold text-transparent sm:text-5xl bg-clip-text bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 h-20">
-            Popular movies
-          </h2>
-        </div>
-        <ul className="grid grid-cols-1 gap-10 box-border justify-items-center sm:grid-cols-2 sm:gap-x-0.5 sm:gap-y-8 lg:grid-cols-5">
-          {popularMovies.map((movie) => (
-            <li key={movie.id} className="shrink-0 snap-center">
-              <Link to={`/movie-detail?movieId=${movie.id}`}>
-                <img
-                  className="w-72 lg:w-52 rounded-xl"
-                  src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                  alt={`${movie.title} poster`}
-                />
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </section>
+      {/* Upcoming movies */}
+      <UpcomingMovies />
     </>
   );
 };
